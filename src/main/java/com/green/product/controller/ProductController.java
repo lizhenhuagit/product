c933d961202b0e66bf34def09ed083cd06d788df
李振华 <zhenhua.li@pharmacodia.com>
2019-09-04 18:15:52 +0800

    swagger 集成

---
+++ src/main/java/com/green/product/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.green.product.controller;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("product")
+@Api(value = "产品接口")
+@Slf4j
+public class ProductController {
+    @ApiOperation(value="保存", notes="")
+    @RequestMapping("save")
+    public String saveUser(){
+    return "success";
+    }
+}
